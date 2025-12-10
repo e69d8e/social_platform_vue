f068d96fc34b74fd3340d0fdb9c86edd86218287
e69d8e <e69d8e1026@gmail.com>
2025-12-10 22:27:01 +0800

    项目初始化

---
+++ src/views/AboutView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="about">
+    <div class="button-row">
+      <el-button>Default</el-button>
+      <el-button type="primary">Primary</el-button>
+      <el-button type="success">Success</el-button>
+      <el-button type="info">Info</el-button>
+      <el-button type="warning">Warning</el-button>
+      <el-button type="danger">Danger</el-button>
+    </div>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+  <main>
+    <div class="container">login</div>
+  </main>
+</template>
+
+<style lang="sass" scoped></style>
